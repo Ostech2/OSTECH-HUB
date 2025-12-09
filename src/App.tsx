7a271a75f60dd68c6bc753da78cba574414b64df
Osbert Kamugisha <kamugishaosbert7@gmail.com>
2025-12-09 20:56:33 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/OSTECH-HUB/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/courses" element={<Courses />} />
